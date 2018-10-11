26a91d071ad9e175d3cdc3662d8e5497f0541963
snapecode <smasnape.gmail.com>
2018-10-10 21:57:07 -0400

    Initial commit

---
+++ weatherfeather/src/MainWindow.vue
@@ -0,0 +1,21 @@
+<template>
+  <Window title="weatherfeather" width="400" height="100" margined v-on:close="exit">
+    <Box padded>
+      <Text>Lets see the weather, we hope it's light as a feather!!</Text>
+      <Box horizontal padded>
+        <TextInput stretchy></TextInput>
+        <Button stretchy>Lets have a look then, shall we??</Button>
+      </Box>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      this.$exit();
+    },
+  },
+};
+</script>
